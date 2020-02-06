c05c710bba64c5638c74279a8856c6d65d25a5a2
SashaPlavi <sasha.bingulac@gmail.com>
2020-02-06 22:29:51 +0100

    loadin pages

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -7,7 +7,15 @@
         protected $params = [];
         public function __construct()
         {
-            print_r($this->getUrl());
+            $url = ($this->getUrl());
+            if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+                $this->currentControler = ucwords($url[0]);
+                //unset 0 index : )
+                unset($url[0]);
+            }
+            require_once '../app/controllers/' . $this->currentControler . '.php';
+
+            $this->currentControler = new $this->currentControler;
         }
         public function getUrl()
         {
